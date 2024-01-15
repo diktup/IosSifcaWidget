7349b5e9a2be0672454b9bec6d880a7a0e9b11f7
Houssem Eddine Fadhli <houssemeddine.fadhli@icloud.com>
2024-01-16 00:47:21 +0100

    Initial commit

--- Package.swift
+++ Package.swift
@@ -14,8 +14,11 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "iossifcawidget"),
+      .target(
+            name: "iossifcawidget",
+            resources: [
+                .copy("Resources")
+            ]),
         .testTarget(
             name: "iossifcawidgetTests",
             dependencies: ["iossifcawidget"]),
