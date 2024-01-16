d8b0b7f94cd4a5431181a5ff4732b2aa79348266
Houssem Eddine Fadhli <houssemeddine.fadhli@icloud.com>
2024-01-17 00:09:36 +0100

    Initial commit

--- Sources/iossifcawidget/WidgetDots.swift
+++ Sources/iossifcawidget/WidgetDots.swift
@@ -9,73 +9,62 @@ import SwiftUI
 import SwiftUIGIF
 
 public struct WidgetDots: View {
-  @Binding public var position: CGPoint
-  @Binding public var isRight: Bool
-  @Binding public var showDots: Bool
-  @Binding public var dialogIndex: Int
+    @Binding public var position: CGPoint
+    @Binding public var isRight: Bool
+    @Binding public var showDots: Bool
+    @Binding public var dialogIndex: Int
 
-  @EnvironmentObject public var animatedWidgetController: AnimatedWidgetController
-  @State public var isShowingDialog = false
+    @EnvironmentObject public var animatedWidgetController: AnimatedWidgetController
+    @State private var imageData: Data? // Added state for image data
+    @State public var isShowingDialog = false
 
-  let imageList = [
-    "https://res.cloudinary.com/sifca/image/upload/v1703254268/flutter-mobile-widget/lord-icon-money.gif",
-    "lhttps://res.cloudinary.com/sifca/image/upload/v1703254270/flutter-mobile-widget/lord-icon-profile.gif",
-    "https://res.cloudinary.com/sifca/image/upload/v1703254272/flutter-mobile-widget/lord-icon-walkie-talkie.gif",
-    "https://res.cloudinary.com/sifca/image/upload/v1703254263/flutter-mobile-widget/lord-icon-chat.gif",
-    "https://res.cloudinary.com/sifca/image/upload/v1703254268/flutter-mobile-widget/lord-icon-joystick.gif",
-    "https://res.cloudinary.com/sifca/image/upload/v1703254263/flutter-mobile-widget/lord-icon-eye.gif",
-    "https://res.cloudinary.com/sifca/image/upload/v1703254270/flutter-mobile-widget/lord-icon-link.gif",
-  ]
+    let imageList = [
+        "https://res.cloudinary.com/sifca/image/upload/v1703254268/flutter-mobile-widget/lord-icon-money.gif",
+        "https://res.cloudinary.com/sifca/image/upload/v1703254270/flutter-mobile-widget/lord-icon-profile.gif",
+        "https://res.cloudinary.com/sifca/image/upload/v1703254272/flutter-mobile-widget/lord-icon-walkie-talkie.gif",
+        "https://res.cloudinary.com/sifca/image/upload/v1703254263/flutter-mobile-widget/lord-icon-chat.gif",
+        "https://res.cloudinary.com/sifca/image/upload/v1703254268/flutter-mobile-widget/lord-icon-joystick.gif",
+        "https://res.cloudinary.com/sifca/image/upload/v1703254263/flutter-mobile-widget/lord-icon-eye.gif",
+        "https://res.cloudinary.com/sifca/image/upload/v1703254270/flutter-mobile-widget/lord-icon-link.gif",
+    ]
 
-  private func loadData(from urlString: String, completion: @escaping (Data?) -> Void) {
-    if let url = URL(string: urlString) {
-      let task = URLSession.shared.dataTask(with: url) { data, response, error in
-        completion(data)
-      }
-      task.resume()
-    } else {
-      // Handle invalid URL case
-      completion(nil)
-    }
-  }
-
-  public var body: some View {
-    ZStack {
-      ForEach(imageList.indices, id: \.self) { index in
-        let angle = Double(index) * (Double.pi / 6) * 1.1 - 1.75
-        let x = position.x + 80 * (isRight ? -cos(angle) : cos(angle))
-        let y = position.y + 80 * sin(angle)
+    public var body: some View {
         ZStack {
+            ForEach(imageList.indices, id: \.self) { index in
+                let angle = Double(index) * (Double.pi / 6) * 1.1 - 1.75
+                let x = position.x + 80 * (isRight ? -cos(angle) : cos(angle))
+                let y = position.y + 80 * sin(angle)
+                ZStack {
 
-          Circle()
-            .fill(Color.white)
-            .frame(width: 34.0, height: 34.0)
-            .shadow(color: Color.black.opacity(0.8), radius: 4, x: 0, y: 0)
-          loadData(
-            from:
-             imageList[index]
-          ) { data in
-            if let imageData = data {
-              GIFImage(data: data)
-                .frame(width: 34.0, height: 34.0)
-                .clipShape(Circle())
-                .shadow(color: Color.black.opacity(0.8), radius: 4, x: 0, y: 0)
-            } else {
-                Text("")
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 34.0, height: 34.0)
+                        .shadow(color: Color.black.opacity(0.8), radius: 4, x: 0, y: 0)
+
+                    if let imageData = imageData {
+                        GIFImage(data: imageData)
+                            .frame(width: 34.0, height: 34.0)
+                            .clipShape(Circle())
+                            .shadow(color: Color.black.opacity(0.8), radius: 4, x: 0, y: 0)
+                    }
+                }
+                .position(x: CGFloat(x), y: CGFloat(y))
+                .gesture(
+                    TapGesture()
+                        .onEnded {
+                            dialogIndex = index
+                            if index == 5 {
+                                AnimatedWidgetManager.toggleTheAnimation(controller: animatedWidgetController)
+                                // Load image data asynchronously
+                                loadData(from: imageList[index]) { data in
+                                    DispatchQueue.main.async {
+                                        imageData = data
+                                    }
+                                }
+                            }
+                        }
+                )
             }
-          }
-
         }
-        .position(x: CGFloat(x), y: CGFloat(y))
-        .gesture(
-          TapGesture()
-            .onEnded {
-              dialogIndex = index
-              if index == 5 {
-                AnimatedWidgetManager.toggleTheAnimation(controller: animatedWidgetController)
-              }
-            })
-      }
     }
-  }
 }
