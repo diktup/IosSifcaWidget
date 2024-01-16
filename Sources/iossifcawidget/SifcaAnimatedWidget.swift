60bee4158db7fb34627cd1713586ca140d431b4a
Houssem Eddine Fadhli <houssemeddine.fadhli@icloud.com>
2024-01-16 16:30:06 +0100

    Initial commit

--- Sources/iossifcawidget/SifcaAnimatedWidget.swift
+++ Sources/iossifcawidget/SifcaAnimatedWidget.swift
@@ -8,36 +8,31 @@
 import SwiftUI
 
 public struct SifcaAnimatedWidget<Content: View>: View {
-    @EnvironmentObject public var animatedWidgetController: AnimatedWidgetController = AnimatedWidgetController()
-    
-    @State public var shakeAmount: CGFloat = 0
+  @EnvironmentObject public var animatedWidgetController: AnimatedWidgetController
 
-    public func getOffset(controller: AnimatedWidgetController) -> CGSize {
-        if animatedWidgetController.isAnimated {
-            let randomX = CGFloat.random(in: 0.0 ... 1.0) * 6 - 1
-            let randomY = CGFloat.random(in: 0.0 ... 1.0) * 6 - 1
-            return CGSize(width: randomX, height: randomY)
-        } else {
-            return CGSize.zero
+  @State public var shakeAmount: CGFloat = 0
+
+  let content: () -> Content
+
+  public init(content: @escaping () -> Content) {
+    self.content = content
+  }
+
+  public var body: some View {
+    if animatedWidgetController.isAnimated {
+      content()
+        .offset(x: shakeAmount, y: shakeAmount * 0.2)
+        .animation(Animation.easeInOut(duration: 0.1).repeatForever())
+        .onAppear {
+          Timer.scheduledTimer(withTimeInterval: 0.4, repeats: true) { _ in
+            self.shakeAmount = self.shakeAmount == 4 ? -4 : 4
+          }
         }
-    }
+        .onDisappear {
 
-    let content: () -> Content
-
-    public var body: some View {
-        if animatedWidgetController.isAnimated {
-            content()
-                .offset(x: shakeAmount, y: shakeAmount * 0.2)
-                .animation(Animation.easeInOut(duration: 0.1).repeatForever())
-                .onAppear {
-                    Timer.scheduledTimer(withTimeInterval: 0.4, repeats: true) { _ in
-                        self.shakeAmount = self.shakeAmount == 4 ? -4 : 4
-                    }
-                }
-                .onDisappear {
-                }
-        } else {
-            content()
         }
+    } else {
+      content()
     }
+  }
 }
