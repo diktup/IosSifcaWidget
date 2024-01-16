2412ebee0539a4a8580bce4db3f5e6b9ea9ab035
Houssem Eddine Fadhli <houssemeddine.fadhli@icloud.com>
2024-01-16 22:24:59 +0100

    Initial commit

--- Sources/iossifcawidget/SifcaAnimatedWidget.swift
+++ Sources/iossifcawidget/SifcaAnimatedWidget.swift
@@ -8,17 +8,16 @@
 import SwiftUI
 
 public struct SifcaAnimatedWidget<Content: View>: View {
-    @EnvironmentObject var animatedWidgetController: AnimatedWidgetController
+    
+    @EnvironmentObject public var animatedWidgetController: AnimatedWidgetController
 
     @State public var shakeAmount: CGFloat = 0
 
     let content: () -> Content
-
-    public init(content: @escaping () -> Content) {
-        self.content = content
-    }
+ 
 
     public var body: some View {
+
         if animatedWidgetController.isAnimated {
             content()
                 .offset(x: shakeAmount, y: shakeAmount * 0.2)
