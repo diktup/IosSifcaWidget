b98ff604d447639360f971ee6ff9ead44a06f556
Houssem Eddine Fadhli <houssemeddine.fadhli@icloud.com>
2024-01-16 23:06:28 +0100

    Initial commit

--- Sources/iossifcawidget/SifcaAnimatedWidget.swift
+++ Sources/iossifcawidget/SifcaAnimatedWidget.swift
@@ -8,37 +8,33 @@
 import SwiftUI
 
 public struct SifcaAnimatedWidget<Content: View>: View {
-    
     let content: () -> Content
-    
-    @StateObject private var animatedWidgetController = AnimatedWidgetController()
+
+    @EnvironmentObject private var animatedWidgetController1: AnimatedWidgetController
+    @StateObject public var animatedWidgetController2: AnimatedWidgetController = AnimatedWidgetController()
+
     @State private var shakeAmount: CGFloat = 0
- 
-   
 
     public init(@ViewBuilder content: @escaping () -> Content) {
         self.content = content
     }
 
     public var body: some View {
- 
-            
-            
-            if animatedWidgetController.isAnimated {
-                content()
-                    .offset(x: shakeAmount, y: shakeAmount * 0.2)
-                    .animation(Animation.easeInOut(duration: 0.1).repeatForever())
-                    .onAppear {
-                        Timer.scheduledTimer(withTimeInterval: 0.4, repeats: true) { _ in
-                            self.shakeAmount = self.shakeAmount == 4 ? -4 : 4
-                        }
+        if animatedWidgetController1.isAnimated {
+            content()
+                .offset(x: shakeAmount, y: shakeAmount * 0.2)
+                .animation(Animation.easeInOut(duration: 0.1).repeatForever())
+                .onAppear {
+                    Timer.scheduledTimer(withTimeInterval: 0.4, repeats: true) { _ in
+                        self.shakeAmount = self.shakeAmount == 4 ? -4 : 4
                     }
-                    .onDisappear {
-                        // Handle any cleanup on disappear
-                    }
-                    .environmentObject(animatedWidgetController)
-            } else {
-                content().environmentObject(animatedWidgetController)
-            }
-     }
+                }
+                .onDisappear {
+                    // Handle any cleanup on disappear
+                }
+                .environmentObject(animatedWidgetController2)
+        } else {
+            content().environmentObject(animatedWidgetController2)
+        }
+    }
 }
