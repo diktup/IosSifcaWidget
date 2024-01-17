e2451bae0d154efedebdac51c87e662624941be9
Houssem Eddine Fadhli <houssemeddine.fadhli@icloud.com>
2024-01-17 22:44:28 +0100

    Initial commit

--- Sources/iossifcawidget/SifcaWidget.swift
+++ Sources/iossifcawidget/SifcaWidget.swift
@@ -81,7 +81,7 @@ public struct SifcaWidget: View {
             if updateDy! > (size.height - 220) {
               top = size.height - 220
             }
-               position = CGPoint(x: left ?? 0, y: top ?? 0)
+               //position = CGPoint(x: left ?? 0, y: top ?? 0)
  
           },
           onPanUpdate: { value in
@@ -98,7 +98,7 @@ public struct SifcaWidget: View {
             if value.location.x > size.width / 2.0 {
               right = true
             }
-                position = CGPoint(x: left ?? 0, y: top ?? 0)
+                //position = CGPoint(x: left ?? 0, y: top ?? 0)
 
           }
         )
