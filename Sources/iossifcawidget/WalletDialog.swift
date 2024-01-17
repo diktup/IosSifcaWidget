0c0731349b06559b5c04846142afea7e6fb3424e
Houssem Eddine Fadhli <houssemeddine.fadhli@icloud.com>
2024-01-17 11:48:53 +0100

    Initial commit

--- Sources/iossifcawidget/WalletDialog.swift
+++ Sources/iossifcawidget/WalletDialog.swift
@@ -250,9 +250,13 @@ public struct WalletDialog: View {
                 HStack {
                     Text("\(Int.random(in: 0 ..< 999))")
                         .font(.headline)
-                    Image("lord-icon-reputation")
-                        .resizable()
-                        .frame(width: 30, height: 30)
+               AsyncImage(url: URL(string: "https://res.cloudinary.com/sifca/image/upload/v1705483169/flutter-mobile-widget/lord-icon-reputation.png")!) { phase in
+                            ((phase.image) ?? Image(systemName: ""))
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 30.0, height: 30.0)
+                                .clipShape(Circle())
+                        }
                 }
                 .padding(8)
                 .background(Color.gray.opacity(0.1))
