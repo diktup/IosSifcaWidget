9f9fa1ed71f46409429ce7f42385fabc0877b775
Houssem Eddine Fadhli <houssemeddine.fadhli@icloud.com>
2024-01-17 12:32:30 +0100

    Initial commit

--- Sources/iossifcawidget/LocaleController.swift
+++ Sources/iossifcawidget/LocaleController.swift
@@ -9,18 +9,22 @@ import Foundation
 import SwiftUI
 
 public class LanguageController: ObservableObject {
-    @Published public var currentLanguage = "en"
-    @Published public var layoutDirection: LayoutDirection = .leftToRight
+  @Published public var currentLanguage = "en"
+  @Published public var layoutDirection: LayoutDirection = .leftToRight
 }
 
 public class Localizer {
+  private static let bundleIdentifier = Bundle.main.bundleIdentifier
 
-    public static func setLanguage(_ language: String, controller: LanguageController) {
-        controller.currentLanguage = language
-        controller.layoutDirection = (language == "ar") ? .rightToLeft : .leftToRight
-    }
+  public static func setLanguage(_ language: String, controller: LanguageController) {
+    controller.currentLanguage = language
+    controller.layoutDirection = (language == "ar") ? .rightToLeft : .leftToRight
+  }
 
-    public static func localizedString(_ key: String, controller: LanguageController) -> String {
-         return NSLocalizedString(key, tableName: nil, bundle: Bundle(identifier: "iossifcawidget"), comment: "")
-    }
+  public static func localizedString(_ key: String, controller: LanguageController) -> String {
+
+    let bundle = Bundle(identifier: bundleIdentifier)
+
+    return NSLocalizedString(key, tableName: nil, bundle: bundle ?? Bundle.main, comment: "")
+  }
 }
